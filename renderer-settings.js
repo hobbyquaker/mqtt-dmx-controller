9d9a2a1e848ce3c51ca9f621e231193b1d4b3300
4lloyd <github@lloydpost.nl>
2019-06-11 22:52:32 +0200

    Added support for MQTT authentication

--- renderer-settings.js
+++ renderer-settings.js
@@ -20,6 +20,8 @@ $('#save').click(() => {
     ipc.send('saveConfig', {
         url: $('#settings-mqtt-url').val(),
         name: $('#settings-mqtt-prefix').val(),
+        mqtt_username: $('#settings-mqtt-username').val(),
+        mqtt_password: $('#settings-mqtt-password').val(),
         address: $('#settings-artnet-host').val(),
         port: $('#settings-artnet-port').val(),
         channels: $('#settings-channels').val()
@@ -31,6 +33,8 @@ $('#save').click(() => {
 ipc.on('config', (event, conf) => {
     $('#settings-mqtt-url').val(conf.url);
     $('#settings-mqtt-prefix').val(conf.name);
+    $('#settings-mqtt-username').val(conf.mqtt_username);
+    $('#settings-mqtt-password').val(conf.mqtt_password);
     $('#settings-artnet-host').val(conf.address);
     $('#settings-artnet-port').val(conf.port);
     $('#settings-channels').val(conf.channels);
